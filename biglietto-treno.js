d1677257c3c6318571bfb4ec29fb26df1532e432
petraalpigiano <petraalpigiano@gmail.com>
2025-04-01 15:43:51 +0200

    add data

--- biglietto-treno.js
+++ biglietto-treno.js
@@ -1,4 +1,15 @@
 // RACCOLTA DATI
+const km = parseInt(
+  prompt("Quale è il numero di km che devi percorrere?", "15")
+);
+const age = parseInt(prompt("Quale è la tua età?", "30"));
+const ticketPrice = km * 0.21;
+const discount20 = (ticketPrice * 20) / 100;
+const discount40 = (ticketPrice * 40) / 100;
+
+console.log("km", km);
+console.log("age", age);
+console.log("ticket prize", ticketPrice);
 
 // SVOLGIMENTO
 
